3af3f1db413ae2bfb270b2924ead2f975057d672
JMBros <justinmichaelbros@gmail.com>
2014-09-05 21:54:15 -0700

    Analyze Text creates a 2D array of sentences with its respective scores

--- js/AnalyzeText.js
+++ js/AnalyzeText.js
@@ -11,20 +11,18 @@
 
 var FIRST_SENTENCE = 1;             // First Sentence Score Increase to be in master branch *******************************************************
 
-function createSentenceArray (paragraphs){
+function createSentenceArray (paragraphs){          // creates a 2D array of sentences with each array containing a sentence and its repective score
     var sentenceArray = [];
-    //console.log(sentenceArray.length);
     for (var block = 0; block < paragraphs.length; block++){
-        var tempSentenceArray = paragraphs[block].split(/[.?;!]/);       ///";"|"."|"?"|"!"/);
-        //console.log(paragraphs[block]);
-        //console.log(tempSentenceArray);
+        var tempSentenceArray = paragraphs[block].split(/[.?;!]/);
         for (var sentenceNumber = 0; sentenceNumber < tempSentenceArray.length; sentenceNumber++){
-            //console.log(tempSentenceArray[sentenceNumber]);
-            if (tempSentenceArray[sentenceNumber] != ""){
+            var currentSentence = tempSentenceArray[sentenceNumber];
+            if (currentSentence != ""){
                 var score = 0;
                 if (sentenceNumber === 0)
                     score += FIRST_SENTENCE;
-                sentenceArray.push([tempSentenceArray[sentenceNumber], score])
+                score += scoreSentence(currentSentence);
+                sentenceArray.push([currentSentence, score])
             }
         }
     }
@@ -32,6 +30,11 @@ function createSentenceArray (paragraphs){
 }/**
  * Created by JMBros on 9/5/14.
  */
+
+function scoreSentence (sentence){                  // helper function to calculate the score of the sentence
+    sentence += "";
+    return 0;
+}
 
 // TEST ENVIRONMENT
 var test = ["NEWPORT, Wales — NATO struggled to find responses to new challenges as it concluded its summit meeting here on Friday, announcing limited steps to deter Russia in Eastern Europe and starting to marshal broader international support to confront radical Islamists in the Middle East.","The alliance said it would establish a rapid-reaction force with an essentially permanent presence in Eastern Europe and would enhance military cooperation with Ukraine.","But the limits of the alliance were visible, too. The United States and Britain used the meeting to try to advance their own emerging policies, especially toward the spread of the Islamic State in Iraq and Syria, while most members shied away from specific commitments to increase military spending."];
